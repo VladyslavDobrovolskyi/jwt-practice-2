7bd51f5d8d9e7e04e62ad860d3e8743df1189900
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-07-13 17:29:18 +0300

    Create User Service

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -1,3 +1,37 @@
-class UserService {}
+const UserModel = require('../models/user-model')
+const UserDto = require('../dtos/user-dto')
+
+const mailService = require('./mail-service')
+const tokenService = require('./token-service')
+const bcrypt = require('bcrypt')
+const uuid = require('uuid')
+
+class UserService {
+  async registation(email, password) {
+    const candidate = UserModel.findOne({ email })
+
+    if (candidate) {
+      throw new Error(`Пользователь с почтовым адресом ${email} уже существует`)
+    }
+
+    const hashPassword = bcrypt.hashSync(password, 3)
+    const activationLink = uuid.v4()
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    })
+    const userDto = new UserDto(user)
+
+    await mailService.sendActivationMail(email, activationLink)
+    const tokens = tokenService.generateTokens({ ...userDto })
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return {
+      ...tokens,
+      user: userDto,
+    }
+  }
+}
 
 module.exports = new UserService()
