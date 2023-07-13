2b4b6b2606abe6900fa762f891233e12a0131a74
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-07-13 17:30:03 +0300

    Create Token Service

--- server/services/token-service.js
+++ server/services/token-service.js
@@ -1,3 +1,32 @@
-class TokenService {}
+const jwt = require('jsonwebtoken')
+const TokenModel = require('../models/token-model')
+
+class TokenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '30m',
+    })
+    const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: '30d',
+    })
+    return {
+      accessToken,
+      refreshToken,
+    }
+  }
+
+  async saveToken(userId, refreshToken) {
+    const tokenData = await TokenModel.findOne({ user: userId })
+
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken
+      return tokenData.save()
+    }
+
+    const token = await TokenModel({ user: userId, refreshToken })
+
+    return token
+  }
+}
 
 module.exports = new TokenService()
