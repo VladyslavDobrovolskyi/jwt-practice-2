480e984960fabd2df62b09b908a9d08bba7702c4
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-07-14 17:13:08 +0300

    Add express-validator

--- server/routes/main-router.js
+++ server/routes/main-router.js
@@ -1,9 +1,15 @@
 const Router = require('express').Router
 const router = new Router()
+const { body } = require('express-validator')
 
 const userController = require('../controllers/user-controller')
 
-router.post('/registration', userController.userRegistration)
+router.post(
+  '/registration',
+  body('email').isEmail(),
+  body('password').isLength({ min: 3, max: 32 }),
+  userController.userRegistration
+)
 router.post('/login', userController.userLogin)
 router.post('logout', userController.userLogout)
 router.get('/refresh', userController.getRefreshToken)
