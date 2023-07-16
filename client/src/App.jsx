1d5935d86dd3fe21d62a9464f0b3d0fb1237900b
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-07-16 20:08:27 +0300

    axios OK 200

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import LoginForm from './components/LoginForm'
 function App() {
 
   return (
+
    <div>
     <h1>Login form</h1>
     <LoginForm/>
--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,35 +2,30 @@ import React, { useContext, useState } from 'react'
 import { Context } from '../main'
 
 const LoginForm = () => {
+
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
   const {store} = useContext(Context) 
+
   return (
-    <form>
-     <input 
+    <div>
+     <input  
      onChange={e => {
-      setEmail(e.target.value,
-        console.log(e.target.value)
-        )
+      setEmail(e.target.value)
     }}
      value = {email}
      type = "text"
       placeholder='Email'
-      autoComplete="current-email"
      />
      <input 
-     onChange={e => setPassword(
-      e.target.value,
-      console.log(e.target.value)
-      )}
+     onChange={e => setPassword(e.target.value)}
      value = {password}
      type = "password"
       placeholder='Пароль'
-      autoComplete="current-password"
      />
      <button onClick={()=> store.login(email, password)}> Логин </button>
      <button onClick={()=> store.registration(email, password)}> Регистрация </button>
-    </form>
+    </div>
   )
   
 }
